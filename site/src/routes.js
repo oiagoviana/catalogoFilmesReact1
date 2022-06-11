8d541b4f741edf89c0f768a8fbdf1b816dfbf8d0
Bruno de Oliveira <brunodeoliveira.22.10@gmail.com>
2022-06-11 13:30:26 -0300

    tela consultar card

--- site/src/routes.js
+++ site/src/routes.js
@@ -4,6 +4,7 @@ import Login from './pages/login'
 import Home from './pages/home'
 import Cadastrar from './pages/cadastrar'
 import Consultar from './pages/consultar'
+import ConsultarCard from './pages/consultarCard'
 import Detalhe from './pages/detalhe'
 
 export default function Index() {
@@ -14,6 +15,7 @@ export default function Index() {
                 <Route path='/admin' element={<Home />} />
                 <Route path='/admin/cadastrar' element={<Cadastrar />} />
                 <Route path='/admin/consultar' element={<Consultar />} />
+                <Route path='/admin/consultarCard' element={<ConsultarCard />} />
                 <Route path='/admin/detalhe' element={<Detalhe />} />
             </Routes>
         </BrowserRouter>
